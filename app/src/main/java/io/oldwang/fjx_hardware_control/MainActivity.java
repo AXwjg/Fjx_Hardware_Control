3f539a1183a8cbd42ddefe558d46d5f8dd540e8e
OldWang <1019622887@qq.com>
2021-04-17 15:37:24 +0800

    1.设置默认页面;

---
+++ app/src/main/java/io/oldwang/fjx_hardware_control/MainActivity.java
@@ -0,0 +1,15 @@
+package io.oldwang.fjx_hardware_control;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
